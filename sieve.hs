f6bb24364bc9d7a323b02c8f0aae4a2b8294f3a5
Andrew Lytle <atlytle+github@gmail.com>
2012-02-04 12:37:55 +0000

    sieve.hs: Impossible problem solution.

--- sieve.hs
+++ sieve.hs
@@ -4,13 +4,22 @@ sieve [] = []
 sieve (1:xs) = sieve xs  -- Remove 1.
 sieve (x:xs) = x:(sieve [w | w <- xs, (w `mod` x) /= 0])
 
+
+--"impossible problem"
+
 primeSums :: Int  -> [(Int, Int)]
 --
-primeSums n = [(x, y) | x <- sieve [2..n], y <- sieve [2..n], x+y == n]
+primeSums n = [(x, y) | x <- primes, y <- primes, x+y == n]
+    where primes = sieve [2..n]
 
-candidate :: Int -> Int
-candidate n 
-    | primeSums n == [] = n
-    | primeSums n /= [] = 0
+discIntegers :: [Int] -> Int -> (Int,[Int])
+--
+discIntegers slist p = (p, [s | s <- slist, (s*s - 4*p) `elem` squares, s/=(p+1)])
+    where 
+        squares = [s*s | s <- [2..(maximum slist)]]
 
---filter (/=0) (map candidate [4..40])
+n=40
+mrS = [s | s <- [4..n], primeSums s == []]
+mrP = [x | x <- map (discIntegers mrS) [4..n*n], length (snd x) == 1]
+
+
